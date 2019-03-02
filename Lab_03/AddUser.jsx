905983f1609711ee048964fa50f63bd41bf627b7
suren vithanage <surenanthonyvithanage@gmail.com>
2019-03-02 13:04:13 +0530

    Lab 03

---
+++ Lab_03/AddUser.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class AddUser extends Component{
+    static get propTypes(){
+        return {
+            addUser: PropTypes.func
+        }
+    }
+
+    constructor(props){
+        super(props);
+    }
+
+    onNameChange(event){
+        event.preventDefault();
+        event.stopPropagation();
+        this.name = event.target.value;
+    }
+
+    onSubmit(event){
+        event.preventDefault();
+        event.stopPropagation();
+        if(this.name){
+            this.props.addUser({ name: this.name});
+            this.name = '';
+        }
+    }
+
+    render(){
+        return <div>
+            <form onSubmit={event => this.onSubmit(event)}>
+                <label htmlFor="Name">Name</label>
+                <input type="text" onChange={event => this.onNameChange(event)}></input>
+                <button type="submit">Add</button>
+            </form>
+        </div>;
+    }
+}
